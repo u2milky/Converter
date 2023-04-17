01c02b50245fb42dc967080e958fc6781cded93e
u2milky <105137274+u2milky@users.noreply.github.com>
2023-04-17 09:38:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
-let number
-let converted
-let base
+let number;
+let converted;
+let base;
 
 window.onload = () => {
-    document.getElementById('errorHandler').hidden = true
+    document.getElementById('errorHandler').hidden = true;
 
-    document.getElementById("butt1").onclick = function () {
-        number = document.getElementById("numberA").value;
-        String(number);
+    document.getElementById("numberA").addEventListener("input", function() {
+        number = this.value;
         console.log(number);
-    }
+    });
 
-
-    document.getElementById("butt2").onclick = function () {
-        base = document.getElementById("base").value;
+    document.getElementById("base").addEventListener("input", function() {
+        base = this.value;
         console.log(base);
-        converted = parseInt(number, base);
-            if(base > 36 || base < 2){
-                document.getElementById('errorHandler').hidden = false
-                document.getElementById("errorHandler").innerHTML = "The base is invalid! Only accepts base between 2 and 36."
-                
-            }else{
-                document.getElementById('errorHandler').hidden = true
-            }
-            
-    }
-
-
-    converted = parseInt(number, base);
+        if (base > 36 || base < 2) {
+            document.getElementById('errorHandler').hidden = false;
+            document.getElementById("errorHandler").innerHTML = "The base is invalid! Only accepts base between 2 and 36.";
+        } else {
+            document.getElementById('errorHandler').hidden = true;
+        }
+    });
 
     document.getElementById("butt3").onclick = function () {
+        converted = parseInt(number, base);
         document.getElementById("converted").innerHTML = "Here's the converted number: " + converted;
+    };
+
+    // get the current hour using the Date object
+    const hour = new Date().getHours();
+
+    // select the body element
+    const body = document.querySelector('body');
+
+    // set the background color based on the time of day
+    if (hour >= 6 && hour < 12) {
+        document.body.style.background = "url('morning.jpg')";
+    } else if (hour >= 12 && hour < 18) {
+        document.body.style.background = "url('afternoon.jpg')";
+    } else {
+        document.body.style.background = "url('night.png')";
     }
 
-}
-
-// get the current hour using the Date object
-const hour = new Date().getHours();
-
-// select the body element
-const body = document.querySelector('body');
-
-// set the background color based on the time of day
-if (hour >= 6 && hour < 12) {
-    document.body.style.background = "url('morning.jpg')";
-} else if (hour >= 12 && hour < 18) {
-    document.body.style.background = "url('morning.jpg')";
-} else {
-    document.body.style.background = "url('night.png')";
-}
-
-
-
-console.log(converted)
+    console.log(converted);
+};
